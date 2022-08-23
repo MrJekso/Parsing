0d5045e6d6a1d5f936fd610c31f5e41ad796f870
user <user@localhost.localdomain>
2022-08-23 23:03:25 +0300

    New rest

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-
+import math
 
 class Parsing:
-	pass
+	def __init__(self):
+		self.name = "Vadim"
+	def get_name(self):
+		return self.name
+	def set_name(self,new_name):
+		self.name = new_name
+	def download_page(self):
+		pass
+
 class Bot:
 	pass
-class Main:
-	pass
 
+class Main:
+	def start(self):
+		#OSNOVA LOGICA
+		main = Parsing()
+		age = 18
+		print(f"age: {age}")
+		print(f"Name:{main.get_name()}")
+		main.set_name("Baron")
+		print(f"Name:{main.get_name()}")
+
+Main().start()
 
