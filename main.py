79360ca25ff50a3beaecb032adf79a43701985eb
root <root@localhost.localdomain>
2022-08-25 14:00:43 +0000

    Overwritten name function and dellete in function main deleted all unnecessary

--- main.py
+++ main.py
@@ -2,26 +2,25 @@ import math
 
 class Parsing:
 	def __init__(self):
-		self.name = "Vadim"
-	def get_name(self):
-		return self.name
-	def set_name(self,new_name):
-		self.name = new_name
+		self.login = ""
+		self.passwd = ""
+		self.server = ""
+	#VADIM
 	def download_page(self):
 		pass
-
+	#VADIM
+	def parsing_page(self):
+		pass
+	def download_data_db(self,data):
+		for i in data:
+			print(f"KURS: {i}")
 class Bot:
 	pass
 
 class Main:
 	def start(self):
 		#OSNOVA LOGICA
-		main = Parsing()
-		age = 18
-		print(f"age: {age}")
-		print(f"Name:{main.get_name()}")
-		main.set_name("Baron")
-		print(f"Name:{main.get_name()}")
-
+		obj = Parsing()
+		obj.download_datat_db(["USD 231","EVRO 433","RUB 42"])
 Main().start()
 
