a6ef6471edc516b7e524c3e5f59753358f14a4a1
parrot <parrot@localhost.localdomain>
2022-08-26 22:39:23 +0300

    New file for parsing from the exchange rate website

---
+++ parsing.py
@@ -0,0 +1,22 @@
+import requests, os
+from bs4 import BeautifulSoup
+
+url = 'https://ru.investing.com/currencies/'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+quotes = soup.find_all('h2', class_='subTitle')
+resthead = soup.find_all('thead')
+table = soup.find_all('tr')
+
+
+for quote in quotes:
+    print(quote.text)
+print('-------------------------------------------------------------')
+for tab in table:
+    res = tab.text.split()
+    print(res, '\n')
+    break
+
+'''    with open("parsing.txt", "a") as file:
+        file.write(str(res))
+'''
