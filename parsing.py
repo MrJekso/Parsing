146a0a06da7555c8a9d1f43487345498f672f728
bysoraxi <bysoraxi@localhost.localdomain>
2022-09-05 13:12:37 +0300

    Ready

--- parsing.py
+++ parsing.py
@@ -6,8 +6,7 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 quotes = soup.find_all('h2', class_='subTitle')
 resthead = soup.find_all('thead')
-table = soup.find_all('tr')
-
+table = soup.find_all('tr', limit=11)
 
 for quote in quotes:
     print(quote.text)
@@ -15,6 +14,8 @@ print('-------------------------------------------------------------')
 for tab in table:
     res = tab.text.split()
     print(res, '\n')
+
+
 
 '''    with open("parsing.txt", "a") as file:
         file.write(str(res))
