1cd8c75b6abc0cc0a0eb6430ac31305fe8771118
bysoraxi <bysoraxi@localhost.localdomain>
2022-09-08 14:13:22 +0300

    Naxyi

--- parsing.py
+++ parsing.py
@@ -1,26 +1,25 @@
 import requests, os
 from bs4 import BeautifulSoup
 
-url = 'https://ru.investing.com/currencies/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
-quotes = soup.find_all('h2', class_='subTitle')
-resthead = soup.find_all('thead')
-table = soup.find_all('tr', limit=11)
 
-#for quote in quotes:
-#    print(quote.text)
-#print('-------------------------------------------------------------')
+def parsing():
+    url = 'https://ru.investing.com/currencies/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    quotes = soup.find_all('h2', class_='subTitle')
+    resthead = soup.find_all('thead')
+    table = soup.find_all('td', limit=90)
 
-index = 0
-for tab in table:
-    if index > 0:
-        res = tab.text.split(',')
-        print(res, '\n')
-    else:
-        index+=1
+    index = 0
+    for tab in table:
+        if index > 0:
+            arr = tab.text.split()
+            print(arr)
+        else:
+            index+=1
+    
+    return arr
+parsing()
+'''with open("parsing.txt", "a") as file:
+    file.w'rite(str(res))'''
 
-
-'''    with open("parsing.txt", "a") as file:
-        file.write(str(res))
-'''
